17fe67ac8f6e4e4f67a0e735ff1dc5bdb7c3901f
Simon Schellaert <me@simonschellaert.com>
2017-11-23 15:14:13 +0100

    Add some comments to the evaluator

--- parser.hs
+++ parser.hs
@@ -5,9 +5,11 @@ import Control.Monad.Reader
 import Control.Monad.State
 import Data.Char
 import qualified Data.Map as Map
+import Data.List
 import Data.Maybe
 import Data.Foldable
 import qualified Text.Read as Read
+import qualified System.Posix.Unistd as Unistd
 
 -- Create the Parser monad we all know and love
 newtype Parser a = Parser { parse :: String -> [(a, String)] }
@@ -106,7 +108,7 @@ first p = Parser $ \inp -> case parse p inp of
                              [] -> []
                              (x:xs) -> [x]
 
--- A parser that applies each given parser and returns its associated value if it succeeds
+-- A parser that applies each of the given parsers and returns its associated value iff it succeeds.
 ops :: [(Parser a, b)] -> Parser b
 ops xs = foldr1 (<|>) [p >> return op | (p,op) <- xs]
 
@@ -153,13 +155,13 @@ block = do indent
            return body
 
 data AExpr = AConst Int
-           | AVar String
+           | AVar Name
            | ASensor Sensor
            | AExpr :+: AExpr
            | AExpr :-: AExpr
            | AExpr :*: AExpr
            | AExpr :/: AExpr
-           deriving (Show)
+           deriving (Show, Eq)
 
 -- A parser for arithmetic expressions
 aExpression :: Parser AExpr
@@ -220,10 +222,10 @@ data Stmt = Assign String AExpr
           deriving (Show)
 
 
-data Sensor = Line | Distance deriving (Show)
-data Direction = Left | Right | Up | Down deriving (Show)
-data Flank = LeftFlank | RightFlank deriving (Show)
-data Duration = VeryShort | Short | Medium | Long | VeryLong | Exact AExpr deriving (Show)
+data Sensor = Line | Distance deriving (Show, Eq)
+data Direction = Left | Right | Up | Down deriving (Show, Eq)
+data Flank = LeftFlank | RightFlank deriving (Show, Eq)
+data Duration = VeryShort | Short | Medium | Long | VeryLong | Exact AExpr deriving (Show, Eq)
 
 data Command = Drive Direction
              | Sleep Duration
@@ -314,7 +316,7 @@ skipStatement = do skipSymbol
 
 
 main = do putStrLn "Please type an arithmetic expression involving +, -, *, /, integers or variablesss"
-          inp <- readFile "obstacles2.txt"
+          inp <- readFile "demo.txt"
           let inp' = preprocess inp
           putStrLn inp'
           let out = parse statementSeq inp'
@@ -328,17 +330,31 @@ main = do putStrLn "Please type an arithmetic expression involving +, -, *, /, i
           --
 
 
+
+
+data Device = Device {
+    sleep        :: Int -> IO (),
+    setRGB       :: Int -> Int -> Int -> Int -> IO (),
+    setMotor     :: Int -> Int -> IO (),
+    readDistance :: IO Int,
+    readLine     :: IO Int
+}
+
+-- The environment used in the evaluator consists of a map from variable names to their corresponding values. This map
+-- is passed around using the state monad transformer and can thus be modified.
 type Name = String
-type Env = Map.Map Name Int
+type Mem  = Map.Map Name Int
 
--- TODO: Use some fancy monad transformers to keep track of the environment and errors
-type Eval a = ReaderT Device (StateT Env IO) a
+-- Next to the mutable state described above, a immutable `Device` is also carried around in the evaluator. This device
+-- is the device that commands are sent to. This lets us easily swap the physical Mbot for the simulator or vice versa.
+type Eval a = ReaderT Device (StateT Mem IO) a
 
+-- This function actually executed the specified evaluator with the specified type. Its result is an IO-action that
+-- executes the program.
+runEval :: Device -> Eval () -> IO ()
+runEval device evaluator  = evalStateT (runReaderT evaluator device) Map.empty
 
-runEval :: Device -> Eval () -> IO ((), Env)
-runEval device ev  = runStateT (runReaderT ev device) Map.empty
-
-
+-- An evaluator for boolean expressions. The operators is our language map nicely to operators built into Haskell.
 evalB :: BExpr -> Eval Bool
 evalB (BConst b)  = return b
 evalB (Not b)     = fmap not (evalB b)
@@ -348,30 +364,74 @@ evalB (a1 :<: a2) = liftM2 (<)  (evalA a1) (evalA a2)
 evalB (a1 :=: a2) = liftM2 (==) (evalA a1) (evalA a2)
 evalB (a1 :>: a2) = liftM2 (>)  (evalA a1) (evalA a2)
 
+-- An evaluator for arithmetic expressions. The operators again map nicely to those built into Haskell.
 evalA :: AExpr -> Eval Int
-evalA (AConst a)  = return a
-evalA (AVar a)    = gets (fromJust . (Map.lookup a))
-evalA (a1 :+: a2) = liftM2 (+) (evalA a1) (evalA a2)
-evalA (a1 :-: a2) = liftM2 (-) (evalA a1) (evalA a2)
-evalA (a1 :*: a2) = liftM2 (*) (evalA a1) (evalA a2)
-evalA (a1 :/: a2) = liftM2 div (evalA a1) (evalA a2)
+evalA (AConst a)         = return a
+evalA (AVar a)           = gets (fromJust . (Map.lookup a)) -- Lookup the value of variable in the environment
+evalA (ASensor Line)     = asks readLine >>= liftIO         -- Return the value of executing readLine on the device
+evalA (ASensor distance) = asks readDistance >>= liftIO     -- Return the value of executing readDistance on the device
+evalA (a1 :+: a2)        = liftM2 (+) (evalA a1) (evalA a2)
+evalA (a1 :-: a2)        = liftM2 (-) (evalA a1) (evalA a2)
+evalA (a1 :*: a2)        = liftM2 (*) (evalA a1) (evalA a2)
+evalA (a1 :/: a2)        = liftM2 div (evalA a1) (evalA a2)
 
-
+-- An evaluator for statements. Note that this evaluator doesn't return anything since the sole purpose of evaluating
+-- a statement is its side effects.
 eval :: Stmt -> Eval ()
-eval (Assign n e) = do v <- evalA e
-                       modify (Map.insert n v)
-eval (Seq xs)     = forM_ xs eval
-eval (If xs)  = forM_ xs attemptBranch
-                where attemptBranch (cond, body) = do isTrue <- evalB cond
-                                                      when isTrue (eval body)
 
+-- Evaluates a sequence of statement by evaluating each statement in turn.
+eval (Seq stmts)     = forM_ stmts eval
 
+-- Evaluates an assignment statement by first evaluating the arithmetic expression on the right and then inserting the
+-- value under the corresponding name in the map of variables.
+eval (Assign name val) = do val' <- evalA val
+                            modify (Map.insert name val')
 
+-- Evaluates an if statement by first evaluating the conditions of each branch, and then evaluating the first branch
+-- whose condition did evaluate to true or nothing if the condition of all branches is false.
+-- Note that an eventual else branch always appears last with `BConst True` as its condition.
+eval (If branches) = do conditions <- mapM (evalB . fst) branches
+                        case (findIndex id conditions) of
+                            Just i   -> eval . snd . (!!i) $ branches
+                            Nothing  -> return ()
 
+-- Evaluates a while statement by evaluating the condition, and iff the condition is true, evaluating the body and then
+-- restarting this procedure.
 eval (While e stmt) = do cond <- evalB e
                          when cond ((eval stmt) >> (eval (While e stmt)))
-eval Skip         = return ()
-eval (Exec cmd) = liftIO (print cmd)
+
+-- Evaluates a skip statement by doing nothing. A skip statement is a comment in the source code so it shouldn't do
+-- anything.
+eval Skip = return ()
+
+-- Evaluates a statement calling the sleep command. Note that the argument of the sleep command is either an arithmetic
+-- expression, in which case the expression is evaluated to determine its value or a built-in constant, in which case
+-- the precise sleep duration is looked up in the association list `durations`.
+eval (Exec (Sleep dur)) = do millis <- (case dur of
+                                          Exact e  -> evalA e
+                                          constant -> return . fromJust . (flip Data.List.lookup durations) $ constant)
+                             asks sleep >>= liftIO . ($millis)
+                             where durations = [(VeryShort, 100), (Short, 250), (Medium, 500),
+                                                (Long, 1000), (VeryLong, 2000)]
+
+
+-- Evaluates a statement calling the drive command. We first lookup the speed of both motors based on the direction and
+-- then send a command to the device with these speeds.
+eval (Exec (Drive dir)) = do let speed = fromJust . (flip Data.List.lookup directions) $ dir
+                             asks setMotor >>= (liftIO . ($speed) . uncurry)
+                             where directions = [(Main.Left, (80, 0)),  (Main.Right, (0, 80)),
+                                                 (Main.Up,   (80, 80)), (Main.Down,  (-80, -80))]
+
+-- Evaluates a statement calling the light command. We first inspect the flank to know which of the 2 light indices to
+-- send to the device, and then evaluate each of the three arithmetic expressions passed as arguments before sending
+-- a command to the device with those values.
+eval (Exec (Light flank r g b)) = do let light = if flank == LeftFlank then 1 else 2
+                                     cmd <- asks setRGB
+                                     liftIO =<< (liftM4 cmd (return light) (evalA r) (evalA g) (evalA b))
+
+-- TODO: Remove this after debugging.
+eval (Exec (Print txt)) = liftIO. putStrLn $ txt
+
 
 preprocess :: String -> String
 preprocess = unlines . (flip evalState [0]) . addMarkers . (++ [""]) . filter (not . all isSpace) . lines
@@ -392,25 +452,24 @@ addMarkers (l:ls) = do indents <- get
                        liftM2 (:) (return l') (addMarkers ls)
 
 
-
-data Device = Device {
-    sleep :: Int -> IO (),
-    setRGB :: Int -> Int -> Int -> Int -> IO (),
-    setMotor :: Int -> Int -> IO ()
-}
-
-
-
+prompt s = do
+    putStr s
+    line <- getLine
+    return (read line)
 
 logDevice = Device {
-    sleep = const (putStrLn "Sleeping"),
-    setRGB = undefined,
-    setMotor = undefined
+    sleep        = \x -> putStrLn ("Sleeping for " ++ show x ++ " milliseconds") >> Unistd.usleep (x * 1000),
+    setRGB       = \l r g b -> putStrLn ("Setting light " ++ show l ++ " to " ++ show r ++ ", " ++ show g ++ ", " ++ show b),
+    setMotor     = \l r -> putStrLn ("Setting motor speed to " ++ show l ++ ", " ++ show r),
+    readDistance = prompt "Enter the measurement of the distance sensor ",
+    readLine     = prompt "Enter the measurement of the line sensor (0, 1, 2 or 3)"
 }
 
 
 
 
+-- In order not to clutter our code with Emoji, we aggregate all the tokens used in our language here. Note that
+-- `symbol` is a function that maps a string to a parser that consumes that string and any remaining whitespace.
 skipSymbol       = symbol "💭"
 whileSymbol      = symbol "🔁"
 ifSymbol         = symbol "❓"
@@ -457,6 +516,4 @@ closeParSymbol   = symbol ")"
 
 
 
--- TODO: The semantics of our else-if are wrong, only the first block with a true condition should be executed, not all of them
 -- TODO: Implement music statement
--- TODO: Rename statemetn to statmentSeq
