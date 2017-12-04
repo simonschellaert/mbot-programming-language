72ae520e74fc0c04d40fcebbc4156ab7384023af
Simon Schellaert <me@simonschellaert.com>
2017-12-04 14:57:55 +0100

    Clean up evaluator a bit more

--- Evaluator.hs
+++ Evaluator.hs
@@ -1,4 +1,6 @@
-module Evaluator where
+module Evaluator (AExpr(..), BExpr(..),
+                  Stmt(..), Command(..), Device(..), Sensor(..), Direction(..), Duration(..), Flank(..),
+                  runStmt) where
 
 import Control.Applicative
 import Control.Monad
@@ -53,7 +55,7 @@ data Flank     = LeftFlank | RightFlank deriving (Show, Eq)
 
 -- By passing a device to the evaluator, the user can choose ad-hoc how to handle each supported command. The evaluator
 -- itself is device-agnostic and just calls the appropriate methods on the passed device. This allows the user to
--- easily switch between running on a physical device (i.e. the MBot itself) or a virtual device (i.e. the simulator)
+-- easily switch between running on a physical device (i.e. the mBot itself) or a virtual device (i.e. the simulator)
 data Device = Device {
     sleep        :: Int -> IO (),                       -- Sleep for the specified number of milliseconds
     setRGB       :: Int -> Int -> Int -> Int -> IO (),  -- Set left (0) or right (1) LED to the specified RGB values
@@ -64,19 +66,24 @@ data Device = Device {
 }
 
 -- The environment used in the evaluator consists of a map from variable names to their corresponding values. This map
--- is passed around using the state monad transformer and can thus be modified.
+-- is passed around using the state monad transformer and can thus be modified when assigning to variables.
 type Name = String
 type Mem  = Map.Map Name Int
 
 -- Next to the mutable state described above, a immutable `Device` is also carried around in the evaluator. This device
--- is the device that commands are sent to. This lets us easily swap the physical Mbot for the simulator or vice versa.
--- Finally, an except monad transformer is used to deal with errors like undefined variables.
+-- is the device that commands are sent to. This lets us easily swap the physical mBot for the simulator or vice versa.
+-- Finally, an except monad transformer is used to gracefully deal with errors like undefined variables.
 type Eval a = ReaderT Device (StateT Mem (ExceptT String IO)) a
 
 -- This function actually executes the specified evaluator with the specified device. Its result is an IO-action that
 -- executes the program.
 runEval :: Device -> Eval () -> IO (Either String ())
 runEval device evaluator  = runExceptT (evalStateT (runReaderT evaluator device) Map.empty)
+
+-- As a convenience method for users of the evaluator, we offer a function takes a device and statement and returns the
+-- IO-action that executes the program with the specified device.
+runStmt :: Device -> Stmt -> IO (Either String ())
+runStmt device stmt = runEval device (eval stmt)
 
 -- An evaluator for boolean expressions. The operators is our language map nicely to operators built into Haskell.
 evalB :: BExpr -> Eval Bool
@@ -99,8 +106,8 @@ evalA (a1 :-: a2)        = liftM2 (-) (evalA a1) (evalA a2)
 evalA (a1 :*: a2)        = liftM2 (*) (evalA a1) (evalA a2)
 evalA (a1 :/: a2)        = liftM2 div (evalA a1) (evalA a2)
 
--- Evaluates a duration by either evaluating the arithmetic or by using one of the built-in constants. The result is an
--- integer representing the duration in milliseconds.
+-- Evaluates a duration. A duration is an arithmetic expression, in which case the expression is evaluated to determine
+-- its value, or a built-in constant.
 evalDuration :: Duration -> Eval Int
 evalDuration (Exact a) = evalA a
 evalDuration constant  = return $ case constant of
@@ -119,11 +126,11 @@ eval (Seq stmts)     = forM_ stmts eval
 
 -- Evaluates an assignment statement by first evaluating the arithmetic expression on the right and then inserting the
 -- value under the corresponding name in the map of variables.
-eval (Assign name val) = do val' <- evalA val
-                            modify (Map.insert name val')
+eval (Assign name e) = do val <- evalA e
+                          modify (Map.insert name val)
 
 -- Evaluates an if statement by first evaluating the conditions of each branch, and then evaluating the first branch
--- whose condition did evaluate to true or nothing if the condition of all branches is false.
+-- whose condition did evaluate to true, or evaluating nothing if the condition of all branches is false.
 -- Note that an eventual else branch always appears last with `BConst True` as its condition.
 eval (If branches) = do conditions <- mapM (evalB . fst) branches
                         case findIndex id conditions of
@@ -135,26 +142,25 @@ eval (If branches) = do conditions <- mapM (evalB . fst) branches
 eval (While e stmt) = do cond <- evalB e
                          when cond (eval stmt >> eval (While e stmt))
 
--- Evaluates a skip statement by doing nothing. A skip statement is a comment in the source code so it shouldn't do
--- anything.
+-- Evaluates a skip statement by doing nothing. A skip statement is a comment in the source code so it simply shouldn't
+-- do anything.
 eval Skip = return ()
 
--- Evaluates a statement calling the sleep command. Note that the argument of the sleep command is either an arithmetic
--- expression, in which case the expression is evaluated to determine its value or a built-in constant, in which case
--- the precise sleep duration is looked up in the association list `durations`.
+-- Evaluates a statement calling the sleep command by first evaluating the duration and then executing the sleep action
+-- for the determined amount of time.
 eval (Exec (Sleep dur)) = do millis <- evalDuration dur
                              asks sleep >>= liftIO . ($millis)
 
--- Evaluates a statement calling the drive command. We first lookup the speed of both motors based on the direction and
--- then send a command to the device with these speeds.
+-- Evaluates a statement calling the drive command. We first lookup the desired speed of both motors based on the
+-- direction and then send a command to the device with these speeds.
 eval (Exec (Drive dir)) = do let speed = fromJust . (`Data.List.lookup` directions) $ dir
                              asks setMotor >>= (liftIO . ($speed) . uncurry)
                              where directions = [(Left, (0, 80)),  (Right, (80, 0)),
                                                  (Up,   (80, 80)), (Down,  (-80, -80))]
 
 -- Evaluates a statement calling the light command. We first inspect the flank to know which of the 2 light indices to
--- send to the device, and then evaluate each of the three arithmetic expressions passed as arguments before sending
--- a command to the device with those values.
+-- send to the device, and then evaluate each of the three arithmetic expressions passed as arguments before sending a
+-- command to the device with those values.
 eval (Exec (Light flank r g b)) = do let light = if flank == LeftFlank then 1 else 2
                                      cmd <- asks setRGB
                                      liftIO =<< liftM4 cmd (return light) (evalA r) (evalA g) (evalA b)
@@ -164,8 +170,7 @@ eval (Exec (Light flank r g b)) = do let light = if flank == LeftFlank then 1 el
 eval (Exec (Sound dur freq)) = do cmd <- asks playTone
                                   liftIO =<< liftM2 cmd (evalA freq) (evalDuration dur)
 
+-- TODO: Remove print command (both in evaluation and in command type)
 eval (Exec (Print txt)) = liftIO. putStrLn $ txt
 
--- TODO: Remove print command (both in evaluation and in Command type)
 -- TODO: Remove `deriving (Show)`
--- TODO: Limit exports
